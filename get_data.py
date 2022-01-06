01b43b396de0ccf9b166ad02ca44746f4109b55e
Vasiliy Zemlyanov <angeldaren@gmail.com>
2022-01-06 20:34:18 +0300

    move log_mel transform into dataloader

--- get_data.py
+++ get_data.py
@@ -1,36 +1,11 @@
 import os
-import numpy as np
 import torch
 from torchaudio import transforms
 from torchaudio.datasets import SPEECHCOMMANDS
 
 
-class SubsetSC(SPEECHCOMMANDS):
-    def __init__(self, path="./", subset: str = None):
-        super().__init__(path, download=True)
-
-        def load_list(filename):
-            filepath = os.path.join(self._path, filename)
-            with open(filepath) as fh:
-                return [
-                    os.path.join(self._path, line.strip()) for line in fh
-                ]
-
-        if subset == "validation":
-            self._walker = load_list("validation_list.txt")
-        elif subset == "testing":
-            self._walker = load_list("testing_list.txt")
-        elif subset == "training":
-            excludes = load_list("validation_list.txt") + load_list("testing_list.txt")
-            excludes = set(excludes)
-            self._walker = [w for w in self._walker if w not in excludes]
-
-    def __getitem__(self, n):
-        waveform, sample_rate, label, _, _ = super().__getitem__(n)
-        return waveform, sample_rate, label
-
-    
-    
+EPS = 1e-9
+SAMPLE_RATE = 16000
 LABELS = [
     'backward',
     'bed',
@@ -70,6 +45,37 @@ LABELS = [
 ]
 
 
+class SubsetSC(SPEECHCOMMANDS):
+    def __init__(self, subset: str, path="./"):
+        super().__init__(path, download=True)
+        self.to_mel = transforms.MelSpectrogram(sample_rate=SAMPLE_RATE, n_fft=1024, f_max=8000, n_mels=40)
+
+        def load_list(filename):
+            filepath = os.path.join(self._path, filename)
+            with open(filepath) as fh:
+                return [
+                    os.path.join(self._path, line.strip()) for line in fh
+                ]
+
+        if subset == "validation":
+            self._walker = load_list("validation_list.txt")
+        elif subset == "testing":
+            self._walker = load_list("testing_list.txt")
+        elif subset == "training":
+            excludes = load_list("validation_list.txt") + load_list("testing_list.txt")
+            excludes = set(excludes)
+            self._walker = [w for w in self._walker if w not in excludes]
+
+    def __getitem__(self, n):
+        waveform, sample_rate, label, _, _ = super().__getitem__(n)
+        if sample_rate != SAMPLE_RATE: 
+            resampler = transforms.Resample(orig_freq=sample_rate, new_freq=SAMPLE_RATE)
+            waveform = resampler(waveform)
+        log_mel = (self.to_mel(waveform) + EPS).log2()
+
+        return log_mel, label
+
+
 _label_to_idx = {label: i for i, label in enumerate(LABELS)}
 _idx_to_label = {i: label for label, i in _label_to_idx.items()}
     
@@ -89,41 +95,10 @@ def pad_sequence(batch):
 
 
 def collate_fn(batch):
-    new_sample_rate = 16000
-    to_mel = transforms.MelSpectrogram(sample_rate=new_sample_rate, n_fft=1024, f_max=8000, n_mels=40)
     tensors, targets = [], []
-    eps = 1e-9
-    for waveform, sample_rate, label in batch:
-        resample = transforms.Resample(orig_freq=sample_rate, new_freq=new_sample_rate)
-
-        tensors += [(to_mel(resample(waveform)) + eps).log2()]
-        targets += [label_to_idx(label)]
-
-    tensors = pad_sequence(tensors)
-    targets = torch.LongTensor(targets)
-
-    return tensors, targets
-
-
-# Test shift augmentation
-# where to put ??
-def collate_fn_train(batch):
-    new_sample_rate = 16000
-    to_mel = transforms.MelSpectrogram(sample_rate=new_sample_rate, n_fft=1024, f_max=8000, n_mels=40)
-    tensors, targets = [], []
-    eps = 1e-9
-    for waveform, sample_rate, label in batch:
-        resample = transforms.Resample(orig_freq=sample_rate, new_freq=new_sample_rate)
-
-        shift = np.random.randint(-1600, 1600)
-        waveform = torch.roll(waveform, shift)
-        if shift > 0:
-            waveform[:shift] = 0
-        elif shift < 0:
-            waveform[-shift:] = 0
-
-        tensors += [(to_mel(resample(waveform)) + eps).log2()]
-        targets += [label_to_idx(label)]
+    for log_mel, label in batch:
+        tensors.append(log_mel)
+        targets.append(label_to_idx(label))
 
     tensors = pad_sequence(tensors)
     targets = torch.LongTensor(targets)
